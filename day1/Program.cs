90c34f5971e840cbfa4a6d1c0c5540d2353464dd
HeikkiHei <heikkihei.ahonen@gmail.com>
2021-12-01 22:51:27 +0200

    lol

---
+++ day1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("data.txt");
+            int[] ints = Array.ConvertAll(lines, int.Parse);
+            Part2(ints);
+
+
+        }
+        static void Part1(int[] ints)
+        {
+            int increase = 0;
+            for (int i = 0; i < ints.Length - 1; i++)
+            {
+                if (ints[i] < ints[i + 1])
+                {
+                    increase++;
+                }
+            }
+            Console.WriteLine(increase);
+        }
+
+        static void Part2(int[] ints)
+        {
+            int increase = 0;
+            for (int i = 0; i < ints.Length - 3; i++)
+            {
+                if (ints[i] + ints[i + 1] + ints[i + 2] < ints[i + 1] + ints[i + 2] + ints[i + 3])
+                {
+                    increase++;
+                }
+            }
+            Console.WriteLine(increase);
+        }
+    }
+}
