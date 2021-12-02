785b73064eaccf290b3f1979cb96cbae47386112
HeikkiHei <heikkihei.ahonen@gmail.com>
2021-12-02 22:12:04 +0200

    shave a bit more

--- day01/part1golf.sh
+++ day01/part1golf.sh
@@ -1,1 +1,1 @@
-s=0;read -d '' -a d<data.txt;for i in "${!d[@]}";{ [[ ${d[i]} -lt ${d[$((i+1))]} ]]&&((s++));};echo $s
+s=0;read -d '' -a d<t;for i in "${!d[@]}";{ [[ d[i] -lt d[$((i+1))] ]]&&((s++));};echo $s
