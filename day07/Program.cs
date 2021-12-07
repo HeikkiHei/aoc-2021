38c4212fcedc95e0913cce43d6decccb46ea3bd8
HeikkiHei <heikkihei.ahonen@gmail.com>
2021-12-07 18:09:37 +0200

    rename vars

--- day07/Program.cs
+++ day07/Program.cs
@@ -10,38 +10,38 @@ namespace day07
     {
         static void Main(string[] args)
         {
-            string[] input = File.ReadAllText("data.txt").Split(",");
-            int[] ints = input.Select(int.Parse).ToArray();
-            Part1(ints);
-            Part2(ints);
+            string[] input = File.ReadAllText("data.txt").Split(",").Select(int.Parse).ToArray();
+            Part1(input);
+            Part2(input);
 
         }
+        
 
-        static double Median(int[] ints)
+        static double Median(int[] input)
         {
-            List<int> sorted = ints.OrderBy(x => x).ToList();
+            List<int> sorted = input.OrderBy(x => x).ToList();
             double mid = (sorted.Count - 1) / 2.0;
             return (sorted[(int)(mid)] + sorted[(int)(mid + 0.5)]) / 2;
         }
 
-        static void Part1(int[] ints)
+        static void Part1(int[] input)
         {
-            double median = Median(ints);
+            double median = Median(input);
             Console.WriteLine(median);
             int sum = 0;
-            foreach (int squid in ints)
+            foreach (int squid in input)
             {
                 sum += Math.Abs(squid - (int)median);
             }
             Console.WriteLine(sum);
         }
 
-        static void Part2(int[] ints)
+        static void Part2(int[] input)
         {
-            int average = Convert.ToInt32(ints.Average());
+            int average = Convert.ToInt32(input.Average());
             Console.WriteLine(average);
             int sum = 0;
-            foreach (int squid in ints)
+            foreach (int squid in input)
             {
                 int n = (Math.Abs(squid - average + 1));
                 int result = (int)(0.5 * n * (n + 1));
