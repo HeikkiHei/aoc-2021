6704d7959e32197577f008fadae73f540c11e283
Heikki Ahonen <heikki.ahonen@centria.fi>
2021-12-09 12:35:15 +0200

    d9p1

--- day09/Program.cs
+++ day09/Program.cs
@@ -7,9 +7,9 @@ namespace day09
   {
     static void Main(string[] args)
     {
-      string[] lines = File.ReadAllLines("sample.txt");
-      int[,] map = new int[5, 10]; // SAMPLE
-                                   // int[,] map = new int[100,100];
+      string[] lines = File.ReadAllLines("data.txt");
+      // int[,] map = new int[5, 10]; // SAMPLE
+      int[,] map = new int[100, 100];
       for (int i = 0; i < lines.Length; i++)
       {
         for (int j = 0; j < lines[i].Length; j++)
@@ -17,7 +17,7 @@ namespace day09
           map[i, j] = Convert.ToInt32(lines[i][j].ToString());
         }
       }
-      PrintMap(map);
+      // PrintMap(map);
       CheckNeighbours(map);
     }
 
@@ -36,43 +36,47 @@ namespace day09
     static void CheckNeighbours(int[,] map)
     {
       int sum = 0;
-      int count = 0;
+      int right = 999;
+      int left = 999;
+      int upper = 999;
+      int down = 999;
       for (int i = 0; i < map.GetLength(0); i++)
       {
-        int up = 999;
-        int down = 999;
-        int left = 999;
-        int right = 999;
+
         for (int j = 0; j < map.GetLength(1); j++)
         {
           int current = map[i, j];
           if (j > 0)
           {
-            up = map[i, j - 1];
+            right = map[i, j - 1];
           }
           if (j < map.GetLength(1) - 1)
           {
-            down = map[i, j + 1];
+            left = map[i, j + 1];
           }
           if (i > 0)
           {
-            left = map[i - 1, j];
+            upper = map[i - 1, j];
           }
           if (i < map.GetLength(0) - 1)
           {
-            right = map[i + 1, j];
+            down = map[i + 1, j];
           }
-          Console.Write(current + " " + i + "," + j + " ");
+
           // CHECK NEIGHBOURS
-          if (current < up && current < left && current < right && current < down)
+          if (current < right && current < upper && current < down && current < left)
           {
-            count++;
-            sum += current;
+            int riskLevel = 1 + current;
+            sum += riskLevel;
           }
+          right = 999;
+          left = 999;
+          upper = 999;
+          down = 999;
         }
-        Console.WriteLine();
+
       }
-      Console.WriteLine(sum + count);
+      Console.WriteLine(sum);
     }
   }
 }
